589b29a717a8cff1449c5003164fc37c98b5e5cf
NickHarvey2 <NickHarvey2@proton.me>
2024-10-21 09:42:31 -0500

    use an alias for step certificate inspect instead of my previous hacked together piping of openssl output to step in a function

--- nick/zsh.nix
+++ nick/zsh.nix
@@ -5,9 +5,6 @@
   # zsh-autoenv.enable = true;
   syntaxHighlighting.enable = true;
   initExtra = ''
-    function tls() {
-      echo | openssl s_client -showcerts -servername $1 -connect $1:443 2>/dev/null | step certificate inspect - --format json | jq $2
-    }
     function jwt() {
       echo -n $1 | cut -d. -f2 | base64 -d - 2>/dev/null | jq
     }
@@ -31,6 +28,7 @@
     scu = "sops -d $FLAKE_DIR/secrets/secrets.yaml > /dev/null"; # add an alias to decrypt sops file and redirect to /dev/null, forcing the smartcard to be unlocked
     bws = "$FLAKE_DIR/bws.sh";
     vpn = "$FLAKE_DIR/vpn.sh";
+    sci = "step certificate inspect --format=json --insecure";
   };
 
   oh-my-zsh = {
