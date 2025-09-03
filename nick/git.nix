1dcd2249206daf5e490b0c67e6b504391d4d2ae5
NickHarvey2 <NickHarvey2@proton.me>
2025-09-03 15:29:05 -0500

    add some helpful git aliases

--- nick/git.nix
+++ nick/git.nix
@@ -11,5 +11,11 @@
     diff.algorithm = "histogram";
     branch.sort = "-committerdate";
     tag.sort = "-version:refname";
+    alias = {
+      graph = "log --all --graph --decorate --oneline";
+      l = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      ignore = "!gi() { local IFS=','; curl -sL https://www.toptal.com/developers/gitignore/api/\"$*\"; }; gi";
+      list-ignore = "curl -sL \"https://www.toptal.com/developers/gitignore/api/$(curl -sL https://www.toptal.com/developers/gitignore/api/list | tr ',' '\n' | gum filter --no-limit | xargs echo -n | tr ' ' ',')\"";
+    };
   };
 }
