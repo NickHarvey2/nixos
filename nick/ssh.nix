30ec74c1b5080f23aef94d9db4aea7969e3ed146
NickHarvey2 <NickHarvey2@proton.me>
2025-09-23 12:27:47 -0500

    remove references to ssh certificate files that are no longer in use

--- nick/ssh.nix
+++ nick/ssh.nix
@@ -13,22 +13,22 @@
     "Mortgage-Research-Center" = {
       match = "User org-134439726 Host github.com";
       identityFile = identities.NickHarveyVu.identityFile;
-      certificateFile = "~/.ssh/id_ed25519-cert_Mortgage-Research-Center.pub";
+      # certificateFile = "~/.ssh/id_ed25519-cert_Mortgage-Research-Center.pub";
     };
     "vu-product-development" = {
       match = "User org-158356857 Host github.com";
       identityFile = identities.NickHarveyVu.identityFile;
-      certificateFile = "~/.ssh/id_ed25519-cert_vu-product-development.pub";
+      # certificateFile = "~/.ssh/id_ed25519-cert_vu-product-development.pub";
     };
     "vu-pdt" = {
       match = "User org-158357197 Host github.com";
       identityFile = identities.NickHarveyVu.identityFile;
-      certificateFile = "~/.ssh/id_ed25519-cert_vu-pdt.pub";
+      # certificateFile = "~/.ssh/id_ed25519-cert_vu-pdt.pub";
     };
     "vu-platform" = {
       match = "User org-158358431 Host github.com";
       identityFile = identities.NickHarveyVu.identityFile;
-      certificateFile = "~/.ssh/id_ed25519-cert_vu-platform.pub";
+      # certificateFile = "~/.ssh/id_ed25519-cert_vu-platform.pub";
     };
     "akamai" = {
       match = "Host *.upload.akamai.com";
