0f6e08ab1db50f194f5fd8c918624cce271e68aa
NickHarvey2 <NickHarvey2@proton.me>
2025-12-06 11:20:27 -0600

    update to new field name

--- nick/git.nix
+++ nick/git.nix
@@ -4,7 +4,7 @@
     "*secrets.yaml diff=sopsdiffer"
     "krb5.conf filter=git-crypt diff=git-crypt"
   ];
-  extraConfig = {
+  settings = {
     diff.sopsdiffer.textconv = "sops -d --config /dev/null";
     push.autoSetupRemove = true;
     pull.rebase = true;
