0910b32975faf699378be136cfb68f8e51351e3a
NickHarvey2 <NickHarvey2@proton.me>
2026-03-01 11:15:50 -0600

    remove comments and unused params from netbird module

--- netbird.nix
+++ netbird.nix
@@ -1,11 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  pkgs,
-  ...
-}: {
-  # TODO <netbird service reconfigure --service-env NB_DISABLE_SSH_CONFIG=true>
   services.netbird.clients.default = {
     port = 51820;
     name = "netbird";
